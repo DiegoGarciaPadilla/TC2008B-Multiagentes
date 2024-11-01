15a63e31ac80aa6c43275cba57e94db96a2a737e
DiegoGarciaPadilla <gpdiego370@outlook.com>
2024-11-01 12:04:51 -0600

    feat(Lab01): part 2

---
+++ unity/Lab01/Lab01/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    // Reference to the player GameObject
+    public GameObject player;
+    // Offset of the camera from the player
+    public Vector3 offset = new Vector3(0, 6, -7);
+
+
+    /// <summary>
+    /// Start is called once before the first execution of Update after the MonoBehaviour is created
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        // Move the camera to follow the player
+        // transform.position = player.transform.position + offset;
+    }
+
+    /// <summary>
+    /// LateUpdate is called once per frame after Update
+    /// </summary>
+    void LateUpdate()
+    {
+        // Move the camera to follow the player
+        transform.position = player.transform.position + offset;
+    }
+}
--- unity/Lab01/Lab01/Assets/Scripts/PlayerController.cs
+++ unity/Lab01/Lab01/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // Speed of the player 
+    public float speed = 0.0f;
+    // Turn speed of the player
+    public float turnSpeed = 0.0f;
+    // Input for the player to move forward
+    public float verticalInput;
+    // Input for the player to turn
+    public float horizontalInput;
+
+    /// <summary>
+    /// Start is called once before the first execution of Update after the MonoBehaviour is created
+    /// </summary>
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
     void Update()
     {
-        transform.Translate(0,0,1);
+        // Get the input for the player to move forward
+        this.verticalInput = Input.GetAxis("Vertical");
+
+        // Horizontal input for the player to move forward
+        this.horizontalInput = Input.GetAxis("Horizontal");
+
+        // Move the player forward
+        // Time.deltaTime: The time in seconds it took to complete the last frame
+        // this.speed: The speed of the player
+        transform.Translate(Vector3.forward * Time.deltaTime * this.speed * this.verticalInput);
+
+        // Turn the player
+        // Time.deltaTime: The time in seconds it took to complete the last frame
+        // this.turnSpeed: The turn speed of the player
+        // this.horizontalInput: The input for the player to move forward
+        transform.Translate(Vector3.right * Time.deltaTime * this.turnSpeed * this.horizontalInput);
+
+        // Rotate the player
+        // Vector3.up: The up direction of the player
+        // Time.deltaTime: The time in seconds it took to complete the last frame
+        // this.turnSpeed: The turn speed of the player
+        // this.horizontalInput: The input for the player to move right
+        transform.Rotate(Vector3.up, Time.deltaTime * this.turnSpeed * this.horizontalInput);
     }
 }

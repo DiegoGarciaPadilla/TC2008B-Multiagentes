87ab4a22f261e8ca00b6701add81e2e9106350a4
DiegoGarciaPadilla <gpdiego370@outlook.com>
2024-11-17 13:48:11 -0600

    chore(Lab06): comment GeneticPathFinder.cs

--- unity/Lab06/Lab06/Assets/Scripts/GeneticPathFinder.cs
+++ unity/Lab06/Lab06/Assets/Scripts/GeneticPathFinder.cs
@@ -4,15 +4,49 @@ using System.Collections.Generic;
 public class GeneticPathFinder : MonoBehaviour
 {
 
+    /// <summary>
+    /// Velocidad de la criatura
+    /// </summary>
     public float creatureSpeed;
+
+    /// <summary>
+    /// Ayuda a mejorar la optimización de genes
+    /// </summary>
     public float pathMultiplier;
+
+    /// <summary>
+    /// Indice del gen visto por la criatura
+    /// </summary>
     int pathIndex = 0;
+
+    /// <summary>
+    /// Genes de la criatura
+    /// </summary>
     public DNA dna;
+
+    /// <summary>
+    /// Indica si la criatura ha terminado su recorrido
+    /// </summary>
     public bool hasFinished = false;
+
+    /// <summary>
+    /// Indica si la criatura ha sido inicializada
+    /// </summary>
     bool hasBeenInitialized = false;
+
+    /// <summary>
+    /// Objetivo de la criatura
+    /// </summary>
     Vector2 target;
+
+    /// <summary>
+    /// Punto siguiente de la criatura
+    /// </summary>
     Vector2 nextPoint;
 
+    /// <summary>
+    /// Evaua si la criatura es apta para pasar sus genes a la siguiente generación
+    /// </summary>
     public float fitness
     {
         get
